dfaf0289cb530cd3a6121cf56ef80523415d5ffa
Lars Stegman <l.s.stegman@student.tudelft.nl>
2016-07-08 16:48:38 +0200

    Initial Commit

---
+++ OvTijdCore/OvTijdCore.h
@@ -0,0 +1,19 @@
+//
+//  OvTijdCore.h
+//  OvTijdCore
+//
+//  Created by Lars Stegman on 08-07-16.
+//  Copyright © 2016 Stegman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OvTijdCore.
+FOUNDATION_EXPORT double OvTijdCoreVersionNumber;
+
+//! Project version string for OvTijdCore.
+FOUNDATION_EXPORT const unsigned char OvTijdCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OvTijdCore/PublicHeader.h>
+
+
